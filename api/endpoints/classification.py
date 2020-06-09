7bff1ee6c77f63431fdb115dc5d82f2bd835376f
stefanodallapalma <stefano.dallapalma0@gmail.com>
2020-06-09 14:31:14 +0200

    update API classify

--- api/endpoints/classification.py
+++ api/endpoints/classification.py
@@ -7,7 +7,8 @@ from ansiblemetrics.metrics_extractor import extract_all
 from flask import abort, Response
 from joblib import load
 
-import random
+MODELS_ROOT = os.path.join(os.path.realpath(__file__).rsplit(os.sep, 3)[0], 'models')
+
 
 def classify(script):
     """
@@ -16,8 +17,10 @@ def classify(script):
 
     :return:  a boolean indicating whether the script has been detected has defective (true) or not )(false).
     """
-    
+
     script = str(script, "utf-8")
+
+    metrics = {}
 
     try:
         metrics = extract_all(StringIO(script))
@@ -25,33 +28,27 @@ def classify(script):
         abort(Response({'Not a valid yaml script:', str(script)}, 400))
 
     test_instance = pd.DataFrame([metrics])
-    
+
     # Load the model
-    model = load(os.path.join('models', 'test_model.joblib'), mmap_mode='r')
-    
-    with open(os.path.join('models', 'test_model.json'), 'r') as infile:
+    model = load(os.path.join(MODELS_ROOT, 'ansible__workshops.pkl'), mmap_mode='r')
+
+    with open(os.path.join(MODELS_ROOT, 'ansible__workshops.json'), 'r') as infile:
         model_features = json.load(infile)
 
-    # Select only numeric features
-    test_instance = test_instance.select_dtypes([np.number])
-    
     # Select same model attributes
     test_instance = test_instance[np.intersect1d(test_instance.columns, model_features)]
-    
+
     # Perform pre-process if any
     if model.named_steps['pre']:
-        test_instances = pd.DataFrame(model.named_steps['pre'].transform(test_instances))
-
-    print(test_instance)
+        test_instance = pd.DataFrame(model.named_steps['pre'].transform(test_instance))
 
     clf = model.named_steps['clf']
 
-    prediction = bool(random.getrandbits(1))
-    #prediction = clf.predict(test_instance)
+    prediction = bool(clf.predict(test_instance)[0])
 
     result = {
         "defective": prediction,
         "metrics": metrics
     }
-    
+
     return result, 200

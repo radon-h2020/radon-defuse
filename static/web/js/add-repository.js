c603d269bfc9eb1eb0a9e05d570a8a541328a139
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2020-10-05 10:50:50 +0200

    Adjusted svc button to switch from github and gitlab. Modified size and colors

--- static/web/js/add-repository.js
+++ static/web/js/add-repository.js
@@ -9,14 +9,18 @@ $("#dropdown-svc-item").click(function(){
   svc_item = $("#dropdown-svc-item").text().trim().toLowerCase()
   index = SVC.indexOf(svc_item)
 
-  $("#dropdown-svc-button").html(capitalizeFirstLetter(SVC[index]))
+  $("#dropdown-svc-button").html('&nbsp;' + capitalizeFirstLetter(SVC[index]))
   $("#dropdown-svc-item").html(capitalizeFirstLetter(SVC[(index+1) % 2]))
   $("#svc-api-token-description").html("This is the personal token to access the " + capitalizeFirstLetter(SVC[index]) + " APIs")
 
   if(index == 0){ // github
+    $("#dropdown-svc-button").addClass('fa-github').removeClass('fa-gitlab')
+    $("#dropdown-svc-button").css("background-color", "black");
     $("#input-svc-identifier").attr("placeholder", "https://github.com/<owner>/<name>");
     $("#svc-identifier-description").html("This is the url to the Github repository.")
   }else{ // gitlab
+    $("#dropdown-svc-button").addClass('fa-gitlab').removeClass('fa-github')
+    $("#dropdown-svc-button").css("background-color", "orange");
     $("#input-svc-identifier").attr("placeholder", "Project ID");
     $("#svc-identifier-description").html("This is unique identifier of the Gitlab project.")
   }

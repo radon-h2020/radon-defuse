36dc530a211a488dc2cc01b0ae7c506dc72ebbee
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-05-17 15:33:42 +0200

    Added modelManager service

---
+++ defuse/src/app/services/model-manager.service.ts
@@ -0,0 +1,46 @@
+import { Injectable,  Output, EventEmitter } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators'
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore'
+
+import { PredictiveModel } from 'app/models/predictive-model.model';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModelManagerService {
+
+    repositoryId: number;
+    models: Observable<PredictiveModel[]>;
+    modelDoc: AngularFirestoreDocument<PredictiveModel>;
+
+    constructor(private httpClient: HttpClient, private store: AngularFirestore){}
+
+    initializeModels(repositoryId: string){
+        this.repositoryId = +repositoryId // to number
+
+        this.models = this.store.collection('models', ref => ref.where('repository_id', '==', this.repositoryId))
+            .snapshotChanges().pipe(map(changes => {
+                return changes.map(item => {
+                    const data = item.payload.doc.data();
+
+                    return {
+                        id: 'id',
+                        name: 'name'
+                    } as PredictiveModel
+                })
+            }))
+    }
+
+    getAll(): Observable<PredictiveModel[]>{
+        return this.models;
+    }
+
+    deleteModel(id: number): Observable<boolean>{
+        this.modelDoc = this.store.doc(`models/${id.toString()}`);
+        this.modelDoc.delete();
+        return of(true)
+    }
+}

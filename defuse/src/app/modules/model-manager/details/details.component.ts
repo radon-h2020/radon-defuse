c14a714f41deecb90824c99e84b39c64633cd487
stefanodallapalma <stefano.dallapalma0@gmail.com>
2021-07-13 12:01:25 +0200

    Updated and cleaned details component

--- defuse/src/app/modules/model-manager/details/details.component.ts
+++ defuse/src/app/modules/model-manager/details/details.component.ts
@@ -11,100 +11,21 @@ export class ModelManagerDetailsComponent{
     @Output() delete = new EventEmitter<string>();
     @Output() dismiss = new EventEmitter<string>();
     @Output() download = new EventEmitter<string>();
+    @Output() chart = new EventEmitter<string>();
 
     onDelete(){
         this.delete.emit();
     }
+
     onDismiss(){
         this.dismiss.emit();
     }
+
     onDownload(){
         this.download.emit();
     }
+
+    onVisualizePerformance(){
+        this.chart.emit(this.item.content.id);
+    }
 }
-
-
-// import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-// import { MatDrawerToggleResult } from '@angular/material/sidenav';
-// import { Subject } from 'rxjs';
-// import { takeUntil } from 'rxjs/operators';
-// import { ModelManagerComponent } from './../model-manager.component';
-// import { ModelsService } from 'app/services/models.service';
-// import { Item } from './../model-manager.component';
-//
-// @Component({
-//     selector       : 'file-manager-details',
-//     templateUrl    : './details.component.html',
-//     encapsulation  : ViewEncapsulation.None,
-//     changeDetection: ChangeDetectionStrategy.OnPush
-// })
-// export class ModelManagerDetailsComponent implements OnInit, OnDestroy
-// {
-//     item: Item;
-//     private _unsubscribeAll: Subject<any> = new Subject<any>();
-//
-//     /**
-//      * Constructor
-//      */
-//     constructor(
-//         private _changeDetectorRef: ChangeDetectorRef,
-//         private _fileManagerListComponent: ModelManagerComponent,
-//         private _fileManagerService: ModelsService
-//     ) {
-//         this.item = { name: 'dependencies', id: '1234435', type:'file'}
-//     }
-//
-//     // -----------------------------------------------------------------------------------------------------
-//     // @ Lifecycle hooks
-//     // -----------------------------------------------------------------------------------------------------
-//
-//     /**
-//      * On init
-//      */
-//     ngOnInit(): void
-//     {
-//         // Open the drawer
-//         this._fileManagerListComponent.matDrawer.open();
-//
-//         // Get the item
-//         this.item = {}
-//
-//         // Mark for check
-//         this._changeDetectorRef.markForCheck();
-//
-//         console.log(this.item)
-//     }
-//
-//     /**
-//      * On destroy
-//      */
-//     ngOnDestroy(): void
-//     {
-//         // Unsubscribe from all subscriptions
-//         this._unsubscribeAll.next();
-//         this._unsubscribeAll.complete();
-//     }
-//
-//     // -----------------------------------------------------------------------------------------------------
-//     // @ Public methods
-//     // -----------------------------------------------------------------------------------------------------
-//
-//     /**
-//      * Close the drawer
-//      */
-//     closeDrawer(): Promise<MatDrawerToggleResult>
-//     {
-//         return this._fileManagerListComponent.matDrawer.close();
-//     }
-//
-//     /**
-//      * Track by function for ngFor loops
-//      *
-//      * @param index
-//      * @param item
-//      */
-//     trackByFn(index: number, item: any): any
-//     {
-//         return item.id || index;
-//     }
-// }

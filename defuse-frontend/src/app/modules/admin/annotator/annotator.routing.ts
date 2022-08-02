f40229113b402eda61ea0bc89599abebc2c9e2d4
stefanodallapalma <stefano.dallapalma0@gmail.com>
2022-08-02 12:33:15 +0200

    Deleted unneeded repository.resolver

--- defuse-frontend/src/app/modules/admin/annotator/annotator.routing.ts
+++ defuse-frontend/src/app/modules/admin/annotator/annotator.routing.ts
@@ -3,7 +3,6 @@ import { CanDeactivateAnnotatorDetails } from 'app/modules/admin/annotator/annot
 import { AnnotatorResolver, CommitResolver, FixedFilesResolver, TagsResolver } from 'app/modules/admin/annotator/annotator.resolver';
 import { AnnotatorComponent } from 'app/modules/admin/annotator/annotator.component';
 import { AnnotatorDetailsComponent } from 'app/modules/admin/annotator/details/details.component';
-import { RepositoriesResolver } from 'app/modules/admin/repositories/repositories.resolver';
 
 export const annotatorRoutes: Route[] = [
     {
@@ -11,7 +10,6 @@ export const annotatorRoutes: Route[] = [
         path     : '',
         component: AnnotatorComponent,
         resolve  : {
-            repositories : RepositoriesResolver,
             commits : AnnotatorResolver,
             fixedFiles: FixedFilesResolver,
             tags: TagsResolver,

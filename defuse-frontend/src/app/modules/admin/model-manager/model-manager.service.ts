be762d3dd68fe3cd9a7d2452294ce2c329c16722
stefanodallapalma <stefano.dallapalma0@gmail.com>
2022-08-04 14:38:41 +0200

    Implemented ModelsService

---
+++ defuse-frontend/src/app/modules/admin/model-manager/model-manager.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of, map } from 'rxjs';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore'
+
+import { PredictiveModel } from 'app/modules/admin/model-manager/predictive-model.model';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModelsService {
+
+    private _modelsCollection: Observable<PredictiveModel[]>;
+    private _models: BehaviorSubject<PredictiveModel[] | null> = new BehaviorSubject([]);
+
+    
+    repositoryId: number;
+    models: Observable<PredictiveModel[]>;
+    modelDoc: AngularFirestoreDocument<PredictiveModel>;
+
+    constructor(private _httpClient: HttpClient, private _firestore: AngularFirestore){
+        this._modelsCollection = _firestore.collection('models').snapshotChanges().pipe(map(changes => {
+            return changes.map(item => {
+                const model = item.payload.doc.data() as PredictiveModel;
+                model.id = item.payload.doc.id
+                return model
+            })
+        }))
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Accessors
+    // -----------------------------------------------------------------------------------------------------
+    get tasks$(): Observable<PredictiveModel[]> {
+        return this._models.asObservable();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+    getModels(): Observable<PredictiveModel[]> {
+        return this._modelsCollection.pipe(
+            map((models) => {
+                this._models.next(models);
+                return models
+            })
+        );      
+      }
+
+    deleteModel(model: PredictiveModel): Observable<any>{
+        const URL = `/backend-api/model?id=${model.id}`;
+        return this._httpClient.delete<any>(URL, {observe:'response'});
+    }
+}

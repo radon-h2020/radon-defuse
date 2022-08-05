bee11b208a5ea7a0df9c3f54ea2322176d3104e4
stefanodallapalma <stefano.dallapalma0@gmail.com>
2022-08-05 15:19:33 +0200

    Refactoring

--- defuse-frontend/src/app/modules/admin/repositories/repositories.service.ts
+++ defuse-frontend/src/app/modules/admin/repositories/repositories.service.ts
@@ -10,7 +10,7 @@ import { Repository, RepositoryPagination } from 'app/modules/admin/repositories
 })
 export class RepositoriesService
 {
-    private repositoriesCollection: Observable<Repository[]>;
+    private _repositoriesCollection: Observable<Repository[]>;
 
     private _pagination: BehaviorSubject<RepositoryPagination | null> = new BehaviorSubject({ length: 0, size: 10, page: 0, lastPage: 0, startIndex: 0, endIndex: 0 } as RepositoryPagination);
     private _repositories: BehaviorSubject<Repository[] | null> = new BehaviorSubject([]);
@@ -20,27 +20,13 @@ export class RepositoriesService
      * Constructor
      */
     constructor(private _httpClient: HttpClient, private _firestore: AngularFirestore) {
-        this.repositoriesCollection = _firestore.collection('repositories', ref => ref.orderBy('full_name','asc')).snapshotChanges().pipe(map(changes => {
-            return changes.map(item => {
-                const data = item.payload.doc.data() as Repository;
-
-                const repo = {
-                    id: data.id,
-                    url: data.url,
-                    full_name: data.full_name,
-                    default_branch: data.default_branch,
-                    // comments_ratio: data["comments_ratio"],
-                    // commit_frequency: data["commit_frequency"],
-                    // core_contributors: data["core_contributors"],
-                    // has_ci: data["has_ci"],
-                    // has_license: data["has_license"],
-                    // iac_ratio: data["iac_ratio"],
-                    // size: data["repository_size"],
-                } as Repository
-
-                return repo
+        this._repositoriesCollection = _firestore.collection('repositories', ref => ref.orderBy('full_name','asc')).snapshotChanges().pipe(
+            map(changes => {
+                return changes.map(item => {
+                    return item.payload.doc.data() as Repository;
+                })
             })
-        }))
+        )
     }
 
 
@@ -64,7 +50,7 @@ export class RepositoriesService
     // -----------------------------------------------------------------------------------------------------
 
     getRepositoriesPage(pageIndex: number = 0, pageSize: number=10): Observable<any> {
-        return this.repositoriesCollection.pipe(
+        return this._repositoriesCollection.pipe(
             map((repositories) => {
                 
                 const length = repositories.length
@@ -125,7 +111,7 @@ export class RepositoriesService
     }
 
     searchRepositories(query: string): Observable<Repository[]> {
-        return this.repositoriesCollection.pipe(
+        return this._repositoriesCollection.pipe(
             tap((repos) => {
                 const filteredRepos = repos.filter(repo => repo.full_name.toLowerCase().includes(query?.toLowerCase())); 
                 this._repositories.next(filteredRepos);

f3cf7e06897eca82a70b1f7c875d007f40225d3f
stefanodallapalma <stefano.dallapalma0@gmail.com>
2022-08-09 14:47:29 +0200

    Refactoring

--- defuse-frontend/src/app/modules/admin/model-manager/model-manager.resolver.ts
+++ defuse-frontend/src/app/modules/admin/model-manager/model-manager.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { catchError, Observable, of, map, throwError } from 'rxjs';
-import { Item, Items } from 'app/modules/admin/model-manager/model-manager.types';
+import { Items } from 'app/modules/admin/model-manager/model-manager.types';
 import { ModelManagerService } from 'app/modules/admin/model-manager/model-manager.service'
 
 @Injectable({
@@ -72,54 +72,6 @@ export class ModelManagerFolderResolver implements Resolve<any>
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Items>
     {
         return this._modelManagerService.getItems(route.paramMap.get('folderId'))
-                   .pipe(
-                       // Error here means the requested task is not available
-                       catchError((error) => {
-
-                           // Log the error
-                           console.error(error);
-
-                           // Get the parent url
-                           const parentUrl = state.url.split('/').slice(0, -1).join('/');
-
-                           // Navigate to there
-                           this._router.navigateByUrl(parentUrl);
-
-                           // Throw an error
-                           return throwError(error);
-                       })
-                   );
-    }
-}
-
-@Injectable({
-    providedIn: 'root'
-})
-export class ModelManagerItemResolver implements Resolve<any>
-{
-    /**
-     * Constructor
-     */
-    constructor(
-        private _router: Router,
-        private _modelManagerService: ModelManagerService
-    )
-    {
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Resolver
-     *
-     * @param route
-     * @param state
-     */
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Item>
-    {
-        return this._modelManagerService.getItemById(route.paramMap.get('id'))
                    .pipe(
                        // Error here means the requested task is not available
                        catchError((error) => {
--- defuse-frontend/src/app/modules/admin/model-manager/model-manager.routing.ts
+++ defuse-frontend/src/app/modules/admin/model-manager/model-manager.routing.ts
@@ -1,9 +1,8 @@
 import { Route } from '@angular/router';
-import { CanDeactivateModelManagerDetails } from 'app/modules/admin/model-manager/model-manager.guards';
 import { ModelManagerComponent } from 'app/modules/admin/model-manager/model-manager.component';
 import { ModelManagerDetailsComponent } from 'app/modules/admin/model-manager/details/details.component';
 import { ModelManagerListComponent } from 'app/modules/admin/model-manager/list/list.component';
-import { ModelManagerFolderResolver, ModelManagerItemResolver, ModelManagerItemsResolver } from './model-manager.resolver';
+import { ModelManagerFolderResolver, ModelManagerItemsResolver } from './model-manager.resolver';
 
 
 export const modelManagerRoutes: Route[] = [
@@ -12,29 +11,23 @@ export const modelManagerRoutes: Route[] = [
         component: ModelManagerComponent,
         children : [
             {
-                path     : 'folders/:folderId',
-                component: ModelManagerListComponent,
-                resolve  : {
-                    item: ModelManagerFolderResolver
-                },
-                children : [
-                    {
-                        path         : 'details/:id',
-                        component    : ModelManagerDetailsComponent,
-                        resolve      : {
-                            item: ModelManagerItemResolver
-                        },
-                        canDeactivate: [CanDeactivateModelManagerDetails]
-                    }
-                ]
-            },
-            {
                 path     : '',
                 component: ModelManagerListComponent,
                 resolve  : {
                     items: ModelManagerItemsResolver
                 }
-            }
+            },
+            {
+                path         : 'details/:id',
+                component    : ModelManagerDetailsComponent,
+            },
+            {
+                path     : 'folders/:folderId',
+                component: ModelManagerListComponent,
+                resolve  : {
+                    item: ModelManagerFolderResolver
+                }
+            },
         ]
     }
 ];

619ef8e7fc3aa4e611ad2f16a65d34a8308b32c6
stefanodallapalma <stefano.dallapalma0@gmail.com>
2022-08-09 17:00:20 +0200

    Adeed analytic charts

--- defuse-frontend/src/app/modules/admin/model-manager/details/details.component.ts
+++ defuse-frontend/src/app/modules/admin/model-manager/details/details.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApexOptions } from 'ng-apexcharts';
 import { Subject, takeUntil } from 'rxjs';
 import { ModelManagerService } from 'app/modules/admin/model-manager/model-manager.service';
 import { Item } from 'app/modules/admin/model-manager/model-manager.types';
@@ -12,6 +13,11 @@ import { Item } from 'app/modules/admin/model-manager/model-manager.types';
 })
 export class ModelManagerDetailsComponent implements OnInit, OnDestroy
 {
+
+    chartAucPR: ApexOptions;
+    chartMcc: ApexOptions;
+    chartPrecisionVsRecall: ApexOptions;
+    data: any;
     item: Item;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
 
@@ -46,6 +52,15 @@ export class ModelManagerDetailsComponent implements OnInit, OnDestroy
                 // Mark for check
                 this._changeDetectorRef.markForCheck();
             });
+
+        // Get the data
+        this._modelManagerService.getAnalytics(this._activatedRoute.snapshot.params.id)
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((data) => {
+                this.data = data;
+                this._prepareChartData();
+                this._changeDetectorRef.markForCheck();
+            });
     }
 
     /**
@@ -60,6 +75,176 @@ export class ModelManagerDetailsComponent implements OnInit, OnDestroy
     // -----------------------------------------------------------------------------------------------------
     // @ Public methods
     // -----------------------------------------------------------------------------------------------------
+
+
+    private _prepareChartData(): void {
+        // AUC-PR
+        this.chartAucPR = {
+            chart  : {
+                animations: {
+                    enabled: false
+                },
+                fontFamily: 'inherit',
+                foreColor : 'inherit',
+                height    : '100%',
+                type      : 'area',
+                sparkline : {
+                    enabled: true
+                }
+            },
+            colors : ['#38BDF8'],
+            fill   : {
+                colors : ['#38BDF8'],
+                opacity: 0.5
+            },
+            series : this.data.aucPR.series,
+            stroke : {
+                curve: 'smooth'
+            },
+            tooltip: {
+                followCursor: true,
+                theme       : 'dark'
+            },
+            xaxis  : {
+                type      : 'category',
+                tickAmount: this.data.aucPR.numReleases,
+            },
+            yaxis  : {
+                labels: {
+                    formatter: (val): string => 'Release ' + val.toString()
+                },
+                max       : 1,
+                min       : 0,
+                show      : false,
+                tickAmount: 20
+            }
+        };
+
+        // MCC
+        this.chartMcc = {
+            chart  : {
+                animations: {
+                    enabled: false
+                },
+                fontFamily: 'inherit',
+                foreColor : 'inherit',
+                height    : '100%',
+                type      : 'area',
+                sparkline : {
+                    enabled: true
+                }
+            },
+            colors : ['#34D399'],
+            fill   : {
+                colors : ['#34D399'],
+                opacity: 0.5
+            },
+            series : this.data.mcc.series,
+            stroke : {
+                curve: 'smooth'
+            },
+            tooltip: {
+                followCursor: true,
+                theme       : 'dark'
+            },
+            xaxis  : {
+                type      : 'category',
+                tickAmount: this.data.mcc.numReleases,
+            },
+            yaxis  : {
+                labels: {
+                    formatter: (val): string => 'Release ' + val.toString()
+                },
+                max       : 1,
+                min       : 0,
+                show      : false,
+                tickAmount: 20
+            }
+        };
+
+        // Precision vs Recall
+        this.chartPrecisionVsRecall = {
+            chart     : {
+                animations: {
+                    enabled: false
+                },
+                fontFamily: 'inherit',
+                foreColor : 'inherit',
+                height    : '100%',
+                type      : 'area',
+                toolbar   : {
+                    show: false
+                },
+                zoom      : {
+                    enabled: false
+                }
+            },
+            colors    : ['#3498DB', '#94A3B8'],
+            dataLabels: {
+                enabled: false
+            },
+            fill      : {
+                colors : ['#AED6F1 ', '#94A3B8'],
+                opacity: 0.5
+            },
+            grid      : {
+                show   : false,
+                padding: {
+                    bottom: -40,
+                    left  : 0,
+                    right : -10
+                }
+            },
+            legend    : {
+                show: false
+            },
+            series    : this.data.precisionVsRecall.series,
+            stroke    : {
+                curve: 'smooth',
+                width: 2
+            },
+            tooltip   : {
+                followCursor: true,
+                theme       : 'dark',
+                x           : {
+                    formatter(value: number): string
+                    {
+                        return `Release: ${value}`;
+                    }
+                },
+            },
+            xaxis     : {
+                axisBorder: {
+                    show: false
+                },
+                labels    : {
+                    offsetY: -20,
+                    // offsetX: -20,
+                    rotate : 0,
+                    style  : {
+                        colors: 'var(--fuse-text-secondary)'
+                    }
+                },
+                tickAmount: this.data.precisionVsRecall.numReleases,
+                tooltip   : {
+                    enabled: false
+                },
+                type      : 'category'
+            },
+            yaxis     : {
+                labels    : {
+                    style: {
+                        colors: 'var(--fuse-text-secondary)'
+                    }
+                },
+                max       : 1,
+                min       : 0,
+                show      : false,
+                tickAmount: 20
+            }
+        };
+
+    }
 
     /**
      * Track by function for ngFor loops
